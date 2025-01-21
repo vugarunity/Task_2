11904abf8fd490a9b69567e0a2ec05a417c58470
vugarunity <159554692+vugarunity@users.noreply.github.com>
2025-01-21 13:16:38 +0400

    Update MathServiceTest.java

--- src/test/java/MathServiceTest.java
+++ src/test/java/MathServiceTest.java
@@ -14,39 +14,32 @@ public class MathServiceTest {
 
     @Test
     void testGetAnswer_whenDiscriminantIsNegative_shouldThrowNotFoundAnswerException() {
-        // Arrange
         int a = 1, b = 2, c = 3; // Дискриминант будет отрицательным
 
-        // Act
         Executable executable = () -> mathService.getAnswer(a, b, c);
 
-        // Assert
         assertThrows(NotFoundAnswerException.class, executable, "Корни не могут быть найдены");
     }
 
     @Test
     void testGetAnswer_whenDiscriminantIsZero_shouldReturnEqualRoots() throws NotFoundAnswerException {
-        // Arrange
         int a = 1, b = -2, c = 1; // Дискриминант равен нулю
 
-        // Act
         Pair result = mathService.getAnswer(a, b, c);
 
-        // Assert
         assertNotNull(result);
         assertEquals(result.first, result.second, "Корни должны быть равны");
     }
 
     @ParameterizedTest
     @CsvSource({
-            "1, -3, 2, 2.0, 1.0", // Пример с двумя корнями
-            "1, 2, 1, -1.0, -1.0", // Пример с одинаковыми корнями
+            "1, -3, 2, 2.0, 1.0", 
+            "1, 2, 1, -1.0, -1.0",
     })
     void testGetAnswer_whenDiscriminantIsPositive_shouldReturnTwoRoots(int a, int b, int c, Double expectedFirstRoot, Double expectedSecondRoot) throws NotFoundAnswerException {
-        // Act
+        
         Pair result = mathService.getAnswer(a, b, c);
-
-        // Assert
+        
         assertNotNull(result);
         assertEquals(expectedFirstRoot, result.first, "Первый корень должен быть верным");
         assertEquals(expectedSecondRoot, result.second, "Второй корень должен быть верным");
